97f9266e93dde0aad5f1ee4442a23686b4fb9e3e
quyphung994 <quypn994@gmail.com>
2023-09-12 21:21:38 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class BTVNBuoi3 {
+    public static void Main() {
+        Console.WriteLine("Ket qua bai 1: " + Bai1("03:03:00PM"));
+
+        string[] name = {"A", "B", "C", "D", "E"};
+        float[] grade = {3.8F, 7.9F, 6.0F, 2.8F, 5.0F};
+        Console.WriteLine("Ket qua bai 2: " + Bai2(name, grade));
+    }
+
+    public static string Bai1(string ampm)
+    {
+        return "";
+    }
+
+    public static string Bai2(string[] name, float[] grade)
+    {
+        return "";
+    }
+}
